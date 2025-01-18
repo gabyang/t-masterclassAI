191d6defbd2393d3f920b8a0374f2ee798865bbf
gabyang <e0725795@u.nus.edu>
2025-01-19 05:05:12 +0800

    improve ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
 import "@/styles/globals.css";
 import clsx from "clsx";
 import { Metadata, Viewport } from "next";
+import { Fira_Code as FontMono, Inter as FontSans } from "next/font/google";
 
 import { Providers } from "./providers";
 
-import { Fira_Code as FontMono, Inter as FontSans } from "next/font/google";
 import NavBar from "@/components/NavBar";
 
 const fontSans = FontSans({
@@ -42,21 +42,28 @@ export default function RootLayout({
   return (
     <html
       suppressHydrationWarning
-      lang="en"
       className={`${fontSans.variable} ${fontMono.variable} font-sans`}
+      lang="en"
     >
       <head />
       <body className={clsx("min-h-screen bg-background antialiased")}>
         <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
           <main className="relative flex flex-col h-screen w-screen">
             <NavBar />
+            {children}
             <h1 className="text-center">
               Disclaimer: This avatar is powered by AI and may produce errors,
               inaccuracies, or hallucinations. Please verify its responses
               independently and remember it is not a human but an AI capable of
               mistakes. Use the information provided responsibly.
             </h1>
-            {children}
+            <h1 className="text-center">
+              This demonstration showcases content from &quot;Structure and
+              Interpretation of Computer Programs,&quot; a textbook employed in
+              CS1101S at the NUS. This course serves as an introductory computer
+              science class for first-year students. The teaching assistant (TA)
+              mentioned here instructs this particular subject.
+            </h1>
           </main>
         </Providers>
       </body>
